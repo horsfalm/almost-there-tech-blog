437531425a645ba5951770ca9292cfe087d97030
BTV <horsfalm@gmail.com>
2022-02-13 15:47:24 +1100

    add comment model and routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+router.get("/", (req, res) => {
+    Comment.findAll()
+        .then((dbCommentData) => res.json(dbCommentData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//Create a comment
+router.post('/', (req, res) => {
+        Comment.create({
+                comment_text: req.body.comment_text,
+                post_id: req.body.post_id,
+                user_id: req.body.user_id
+            })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    });
+
+
+module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 
 router.get("/", (req, res) => {

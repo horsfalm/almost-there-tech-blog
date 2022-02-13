f5e362b43846a78e9daf1be86b6d18f49370d86a
BTV <horsfalm@gmail.com>
2022-02-13 20:17:05 +1100

    session secret

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: 'Super secret secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
